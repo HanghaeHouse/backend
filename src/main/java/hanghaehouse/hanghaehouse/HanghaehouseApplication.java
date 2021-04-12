184af011e8018400fa3e44e5e4565b11e272d712
jongwanhong <infinity4869@gmail.com>
2021-04-12 10:09:44 +0900

    env set

---
+++ src/main/java/hanghaehouse/hanghaehouse/HanghaehouseApplication.java
@@ -0,0 +1,13 @@
+package hanghaehouse.hanghaehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hanghaehouse/hanghaehouse/HanghaehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package hanghaehouse.hanghaehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
