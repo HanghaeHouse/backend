d90445538a853f34bce00979b6d19ecf5420e905
Sanggu <rkdtkdrn93@naver.com>
2021-04-12 17:08:05 +0900

    04.12 로그인 회원가입
    04.12 로그인 회원가입

---
+++ src/main/java/hanghaehouse/hanghaehouse/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package hanghaehouse.hanghaehouse.repository;
+
+import hanghaehouse.hanghaehouse.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    Optional<User> findByEmail(String email);
+
+}
